054cc9d60efd47df5ea985662eb9e53d852a1e7b
gaganso <gagan.somashekar@gmail.com>
2025-08-24 00:15:29 +0000

    update azurerm provider and remove unnecessary providers

--- scripts/terraform/providers.tf
+++ scripts/terraform/providers.tf
@@ -2,16 +2,8 @@ terraform {
   required_version = ">=1.6"
 
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~>1.5"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
       version = "~>3.0"
     }
   }
