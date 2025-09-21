5fc2b3befec5456fc799940f26f90ee94cff37a3
gaganso <gagan.somashekar@gmail.com>
2025-09-21 17:35:21 +0000

    support dynamic cluster size and update the terraform scripts

--- scripts/terraform/outputs.tf
+++ scripts/terraform/outputs.tf
@@ -1,19 +1,7 @@
-output "public_ip_address_1" {
-  value = azurerm_linux_virtual_machine.aiopslab_vm_1.public_ip_address
+output "controller_public_ip" {
+  value = azurerm_public_ip.controller.ip_address
 }
 
-output "public_ip_address_2" {
-  value = azurerm_linux_virtual_machine.aiopslab_vm_2.public_ip_address
-}
-
-output "key_data_1" {
-  value = azapi_resource_action.aiopslab_ssh_public_key_gen_1.output.privateKey
-}
-
-output "key_data_2" {
-  value = azapi_resource_action.aiopslab_ssh_public_key_gen_2.output.privateKey
-}
-
-output "username" {
-  value = var.username
+output "worker_public_ips" {
+  value = [for vm in azurerm_public_ip.workers : vm.ip_address]
 }
